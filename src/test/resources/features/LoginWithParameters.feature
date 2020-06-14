bd357db59cf30ca588cea62face970c22ced7736
“ahmet1972” <ahmeterdogan94@gmail.com>
2020-06-14 21:05:04 +0100

    first commit

---
+++ src/test/resources/features/LoginWithParameters.feature
@@ -0,0 +1,15 @@
+Feature: Login as different users
+
+
+  Scenario: login as a driver user
+    Given the user is on the login page
+    When the user logs in using "user1" and "UserUser123"
+    Then the user should be able to login
+    And the title contains "Dashboards"
+
+    #Homework
+  Scenario: login as a driver user
+    Given the user logged in as "store manager"
+    Then the user should be able to login
+    And the title contains "Dashboards"
+    #driver,sales manager,store manager
---
+++ target/default-html-reports/report.js
@@ -0,0 +1,246 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/Contacts.feature");
+formatter.feature({
+  "name": "Contacts page",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "login as a given user \u003cuserType\u003e",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.step({
+  "name": "the user is on the login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "the user logs in using following credentials",
+  "keyword": "When ",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "\u003cuserType\u003e"
+      ]
+    },
+    {
+      "cells": [
+        "password",
+        "UserUser123"
+      ]
+    },
+    {
+      "cells": [
+        "firstname",
+        "\u003cfirstName\u003e"
+      ]
+    },
+    {
+      "cells": [
+        "lastname",
+        "\u003clastName\u003e"
+      ]
+    }
+  ]
+});
+formatter.step({
+  "name": "the user should be able to login",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "userType",
+        "firstName",
+        "lastName"
+      ]
+    },
+    {
+      "cells": [
+        "user1",
+        "John",
+        "Doe"
+      ]
+    },
+    {
+      "cells": [
+        "salesmanager101",
+        "Peyton",
+        "Harber"
+      ]
+    },
+    {
+      "cells": [
+        "storemanager85",
+        "Marcella",
+        "Huels"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "login as a given user user1",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials",
+  "rows": [
+    {},
+    {},
+    {},
+    {}
+  ],
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_logs_in_using_following_credentials(java.util.Map\u003cjava.lang.String, java.lang.String\u003e)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to login",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_user_should_be_able_to_login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "login as a given user salesmanager101",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials",
+  "rows": [
+    {},
+    {},
+    {},
+    {}
+  ],
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_logs_in_using_following_credentials(java.util.Map\u003cjava.lang.String, java.lang.String\u003e)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to login",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_user_should_be_able_to_login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "login as a given user storemanager85",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@wip"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user is on the login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_user_is_on_the_login_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user logs in using following credentials",
+  "rows": [
+    {},
+    {},
+    {},
+    {}
+  ],
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.ContactsStepDefs.the_user_logs_in_using_following_credentials(java.util.Map\u003cjava.lang.String, java.lang.String\u003e)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user should be able to login",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.vytrack.step_definitions.LoginStepDefs.the_user_should_be_able_to_login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
